d0acb1bff4a13091f28319d452fb32817c6e5e4e
Yevgeny Iliyn <eugene.ilyin@gmail.com>
2017-01-25 16:20:10 +0400

    Update to new mORMot Prepare method

--- BoilerplateHTTPServer.pas
+++ BoilerplateHTTPServer.pas
@@ -1217,9 +1217,9 @@ begin
     ((Context.URL = '') or (Context.URL = '/')) then
     with Context do
       if bpoDelegateIndexToInheritedDefault in FOptions then
-        Prepare('/Default', Method, InHeaders, InContent, InContentType)
+        Prepare('/Default', Method, InHeaders, InContent, InContentType, '')
       else
-        Prepare('/index.html', Method, InHeaders, InContent, InContentType);
+        Prepare('/index.html', Method, InHeaders, InContent, InContentType, '');
 
   SplitURL(Context.URL, Path, Ext, bpoEnableCacheBusting in FOptions);
 
@@ -1311,13 +1311,13 @@ begin
     if bpoDelegate404ToInherited_404 in LOptions then
     begin
       with Context do
-        Prepare('/404', Method, InHeaders, InContent, InContentType);
+        Prepare('/404', Method, InHeaders, InContent, InContentType, '');
       inherited Request(Context);
       ContentType := ContentTypeWithoutCharset(Context.OutContentType);
       Result := HTTP_NOTFOUND;
     end else begin
       with Context do
-        Prepare('/404.html', Method, InHeaders, InContent, InContentType);
+        Prepare('/404.html', Method, InHeaders, InContent, InContentType, '');
       Asset := FAssets.Find('/404.html');
       if Asset <> nil then
       begin
